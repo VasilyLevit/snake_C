f692d648f3075417f5c91dfe5b9833fccd12aba8
VasilyLevit <vasily.levit@gmail.com>
2024-06-13 08:21:58 +0800

    add moving

--- snake_04_01.c
+++ snake_04_01.c
@@ -1,18 +1,32 @@
+/* Змейка 
+представляет собой голову(один элемента) и хвост(массив координат элементов тела)*/
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>  //для функции sleep, read
+#include <unistd.h> //для функции sleep, read
 #include <ncurses.h>
 
-// границы поля для перемещения
+// границы поля перемещения
 #define MAX_X 15
 #define MAX_Y 15
 
-enum {LEFT=1, UP, RIGHT, DOWN, STOP_GAME = 'q'};
+enum {LEFT = 1, UP, RIGHT, DOWN, STOP_GAME = KEY_F(10)};
 
-/* Хвост это массив состоящий из координат
-x,y - т.е. это элементы тела змейки (одна клетка)*/
-typedef struct tail_t {
-	int x;
+// Коды управления змейкой и присвоенные клавиши хранятся в структурах. Змейка управляется нажатием клавиш «вверх», «вниз», «вправо», «влево».
+struct control_buttons
+{
+    int down;
+    int up;
+    int left;
+    int right;
+} control_buttons;
+
+struct control_buttons default_controls= {KEY_DOWN, KEY_UP, KEY_LEFT, KEY_RIGHT};
+
+// Структура элемента хвоста
+typedef struct tail_t
+{
+	int x; // координата элемента хвоста
 	int y;
 } tail_t;
 
@@ -20,15 +34,17 @@ typedef struct tail_t {
 x,y - координаты текущей позиции головы
 direction - направление движения
 tsize - размер хвоста
-*tail -  ссылка на хвост */	
+*tail -  ссылка на хвост */
 typedef struct snake_t
 {
-	int x;  // координата головы
-	int y;  // координата головы
-	struct tail_t * tail;  // множество элементов хвоста (ссылка на хвост).
-	size_t tsize;  // длина хвоста змеи, т.е. количество элементов хвоста (без головы)
+	int x;				 // координаты головы
+	int y;
+	struct tail_t *tail; // множество элементов хвоста (ссылка на хвост).
+	size_t tsize;		 // длина хвоста змеи, т.е. количество элементов хвоста (без головы)
 	// struct control_buttons controls;
-}  snake_t;
+	// int direction;
+} snake_t;
+
 
 // --> x направление оси х слева направо
 // || Y напрвление оси y сверх вниз
@@ -37,12 +53,12 @@ typedef struct snake_t
 // функция инициализации змейки (координаты головы, начальная длина змейки)
 struct snake_t initSnake(int x, int y, size_t tsize)
 {
-	snake_t snake;  // создаём переменную snake
+	snake_t snake; // создаём переменную snake
 	snake.x = x;
 	snake.y = y;
 	snake.tsize = tsize;
-	snake.tail = (tail_t *) malloc (sizeof(tail_t) * 100); // выделяем динамическую память под 100 элементов хвоста змеи (пока она заполнена мусором)
-	// инициализируем хвост - создаем количество элеметов по длине змеи 
+	snake.tail = (tail_t *)malloc(sizeof(tail_t) * 100); // выделяем динамическую память под 100 элементов хвоста змеи (пока она заполнена мусором)
+	// инициализируем хвост - создаем количество элеметов по длине змеи
 	for (int i = 0; i < tsize; ++i)
 	{
 		snake.tail[i].x = x + i + 1; //  заполняем координты элементов хвоста возрастающими значениями (+1 - поправка на голову (она имеет координату 0), начинаем со следующего элемента)
@@ -51,7 +67,6 @@ struct snake_t initSnake(int x, int y, size_t tsize)
 	return snake;
 }
 
-	
 // @**
 // печать смейки в виде матрицы
 void printSnake(snake_t snake)
@@ -68,17 +83,17 @@ void printSnake(snake_t snake)
 	// рисуем хвос
 	for (int i = 0; i < snake.tsize; ++i)
 		matrix[snake.tail[i].x][snake.tail[i].y] = '*';
-			
+
 	// выводим матрицу на экран
 	for (int j = 0; j < MAX_Y; ++j)
 	{
 		for (int i = 0; i < MAX_X; ++i)
 			printf("%c", matrix[i][j]);
-			printf("\n");
+		printf("\n");
 	}
 }
 
-// шаг влево	
+// шаг влево
 // <--  @** tsize = 2
 //     @**
 
@@ -89,30 +104,49 @@ void printSnake(snake_t snake)
 // двигаем голову, а остальное за ней
 snake_t moveLeft(snake_t snake)
 {
-	for (int i = snake.tsize - 1; i > 0; i--)  //идем по хвосту с конца (чтобы не затереть раньше времени координаты левых элементов)
-		snake.tail[i] = snake.tail[i-1]; // присваивая координаты каждого левого элемента правому
-	snake.tail[0].x = snake.x;  //нулевому элементу хвоста присваиваем координаты головы
+	for (int i = snake.tsize - 1; i > 0; i--) // идем по хвосту с конца (чтобы не затереть раньше времени координаты левых элементов)
+		snake.tail[i] = snake.tail[i - 1];	  // присваивая координаты каждого левого элемента правому
+	snake.tail[0].x = snake.x;				  // нулевому элементу хвоста присваиваем координаты головы
 	snake.tail[0].y = snake.y;
-	snake.x = snake.x - 1;	 //голове присваиваем координату левее на один шаг
-	//контролируем, чтобы не появилиь отрицательные индексы
-	if (snake.x < 0) 
-		snake.x = MAX_X - 1;  //при подходе к левому краю появится справа
-	return snake; // возвращаем смещенную змейку
-}	
-	
+	snake.x = snake.x - 1; // голове присваиваем координату левее на один шаг
+	// контролируем, чтобы не появилиь отрицательные индексы
+	if (snake.x < 0)
+		snake.x = MAX_X - 1; // при подходе к левому краю появится справа
+	return snake;			 // возвращаем смещенную змейку
+}
+
+// Управление движением
+// Для изменения направления движения используется функция:
+// void changeDirection(snake_t* snake, const int32_t key).
+void changeDirection(snake_t* snake, const int32_t key)
+{
+    if (key == snake->controls.down)
+        snake->direction = DOWN;
+    else if (key == snake->controls.up)
+        snake->direction = UP;
+    else if (key == snake->controls.right)
+        snake->direction = RIGHT;
+    else if (key == snake->controls.left)
+        snake->direction = LEFT;
+}
+
+// функция проверки корректности выбранного направления
+int checkDirection(snake_t* snake, int32_t key)
+
 int main()
 {
-	snake_t snake = initSnake(10, 5, 2);  //создаем змейку
-	printSnake(snake);	
+	snake_t snake = initSnake(10, 5, 2); // создаем (инициализируем) змейку
+	printSnake(snake);
 	char key_pressed = 0;
 
-	while(key_pressed != STOP_GAME) //сдвигается пока не уедет
-	{				
-		snake = moveLeft(snake); //движение змейки
-		sleep(1);  // задержка перед смещением змейки (иллюзия движения)
-		system("clear"); // очистка экрана, в stdlib (для Win "cls")
+	while (key_pressed != STOP_GAME) // сдвигается пока не уедет
+	{
+		snake = moveLeft(snake); // движение змейки
+		sleep(1);				 // задержка перед смещением змейки (иллюзия движения)
+		system("clear");		 // очистка экрана, в stdlib (для Win "cls")
 		printSnake(snake);
-		key_pressed = read(fileno(stdin), &key_pressed, 1); // Считываем клавишу STDIN_FILENO
+		// key_pressed = read(fileno(stdin), &key_pressed, 1); // Считываем клавишу STDIN_FILENO
+		read(fileno(stdin), &key_pressed, 4);
 	}
 	return 0;
 }
